53840d283449a801ff87754812bca2d3dcfdb12b
David PAGNON <contact@david-pagnon.com>
2026-05-26 20:58:06 +0200

    footer link fix

--- Content/website/build.py
+++ Content/website/build.py
@@ -377,7 +377,7 @@ HTML_TEMPLATE = """\
         <!-- Footer -->
         <footer class="footer">
             <div class="footer-content">
-                <p><a href="https://github.com/perfanalytics/pose2sim" target="_blank">Pose2Sim_Blender GitHub</a> (⭐please leave a star!⭐) &bull;
+                <p><a href="https://github.com/davidpagnon/Pose2Sim_Blender" target="_blank">Pose2Sim_Blender GitHub</a> (⭐please leave a star!⭐) &bull;
                    <a href="#how-to-cite">Cite Pose2Sim_Blender</a>
                 </p>
                 <p>Open-source markerless motion capture &bull; BSD-3-Clause License</p>
