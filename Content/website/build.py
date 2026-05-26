e23e3b48146df2c737ebf0aef532ea5411bf391d
David PAGNON <contact@david-pagnon.com>
2026-05-26 10:48:47 +0200

    typo

--- Content/website/build.py
+++ Content/website/build.py
@@ -339,7 +339,7 @@ HTML_TEMPLATE = """\
                 <a href="https://perfanalytics.github.io/pose2sim/#pose2sim" 
                    class="docs-switch" title="Pose2Sim documentation">Pose2Sim</a>
                 <span class="docs-switch-sep">|</span>
-                <a href=https://davidpagnon.github.io/Sports2D/#sports2d"
+                <a href="https://davidpagnon.github.io/Sports2D/#sports2d"
                    class="docs-switch" title="Sports2D documentation">Sports2D</a>
                 <span class="docs-switch-sep">|</span>
                 <a href="https://davidpagnon.github.io/Pose2Sim_Blender/#pose2sim-blender/"
